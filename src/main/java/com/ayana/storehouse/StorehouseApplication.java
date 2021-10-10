2bfeda3aa5d045542d0457c1306f0a6ce9b3d9c3
vikramthakur <vikram.thakur788@gmail.com>
2021-10-10 20:26:12 +1100

    adding inital app

---
+++ src/main/java/com/ayana/storehouse/StorehouseApplication.java
@@ -0,0 +1,13 @@
+package com.ayana.storehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorehouseApplication.class, args);
+	}
+
+}
